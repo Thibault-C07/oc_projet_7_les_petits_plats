cec98b1481332665b50a265505b7ab552ce4326f
Thibault Cotel <cotelthibault@gmail.com>
2023-12-23 20:25:59 +0100

    fix: final version second algo loop for()

--- search/searchBar.js
+++ search/searchBar.js
@@ -31,11 +31,53 @@ function searchBar() {
   }
 }
 
+function searchByFilters(selectedFilters) {
+  const userInput = searchInput.value.toLowerCase();
+  let results = [];
+
+  if (userInput.length >= 3) {
+    // Recherche par texte
+    results = recipes.filter((recipe) => {
+      const titleMatch = recipe.name.toLowerCase().includes(userInput);
+      const ingredientsMatch = recipe.ingredients.some((ingredient) =>
+        ingredient.ingredient.toLowerCase().includes(userInput)
+      );
+      const descriptionMatch = recipe.description
+        .toLowerCase()
+        .includes(userInput);
+      return titleMatch || ingredientsMatch || descriptionMatch;
+    });
+  } else {
+    // Si la recherche par texte est vide, utilisez la liste complète
+    results = recipes.slice();
+  }
+
+  // Filtrage supplémentaire en fonction des filtres sélectionnés
+  for (const filter of selectedFilters) {
+    results = results.filter((recipe) => {
+      const ingredientsMatch = recipe.ingredients.some((ingredient) =>
+        ingredient.ingredient.toLowerCase().includes(filter)
+      );
+      const applianceMatch = recipe.appliance.toLowerCase().includes(filter);
+      const ustensilsMatch = recipe.ustensils.some((ustensil) =>
+        ustensil.toLowerCase().includes(filter)
+      );
+
+      return ingredientsMatch || applianceMatch || ustensilsMatch;
+    });
+  }
+
+  updateSearchResults(results);
+  fillCards(results);
+}
+
 function updateSearchResults(results) {
+  // Appel des 3 fonctions pour extraire les différentes listes à partir des résultats de recherche
   const uniqueIngredients = getUniqueIngredients(results);
   const uniqueAppliances = getUniqueAppliances(results);
   const uniqueUstensils = getUniqueUstensils(results);
 
+  // Mise à jour des dropdowns
   updateDropdownOptions(1, uniqueIngredients, "ingredient");
   updateDropdownOptions(2, uniqueAppliances, "appliance");
   updateDropdownOptions(3, uniqueUstensils, "ustensil");
@@ -46,49 +88,59 @@ function updateSearchResults(results) {
     const isInIngredients = uniqueIngredients.includes(filter);
     const isInAppliances = uniqueAppliances.includes(filter);
     const isInUstensils = uniqueUstensils.includes(filter);
+    // Check pour chaque filtre si les différents éléments sont présents (ingrédients, appareils et ustensiles)
     if (isInIngredients || isInAppliances || isInUstensils) {
       const dropdownElement = findDropdownElementByText(filter, containers);
       if (dropdownElement) {
+        // Mise à jour des la mise en page des éléments
         updateSelectedItemLayout(dropdownElement);
       }
     }
   });
 }
 
+/* Fonction de recherche par texte à l'intérieur des dropdowns */
 function findDropdownElementByText(text, containers) {
   for (const container of containers) {
+    // Sélection des éléments p des conteneurs des dropdowns
     const allDropdownElements = container.querySelectorAll("p");
 
     for (const element of allDropdownElements) {
+      // Check de l'élément tapé dans la barre de recherche
       if (
         element.textContent.trim().toLowerCase() === text.trim().toLowerCase()
       ) {
+        // Renvoie de l'élément
         return element;
       }
     }
   }
+  // Renvoie null s'il n'y a rien
   return null;
 }
 
+/* Reset des recettes */
 function resetRecipes() {
   fillCards(recipes);
   updateRecipeCount();
 }
 
+/* Fonction de mise à jour du contenu des dropdowns avec les options générées (texte ou objets) */
 function updateDropdownOptions(dropdownNumber, options, property) {
+  // Choix du dropdown (1, 2 et 3)
   const dropdownId = `dd${dropdownNumber}-list`;
   const dropdown = document.getElementById(dropdownId);
-
+  // Si le dropdown n'est pas trouvé alors message d'erreur
   if (!dropdown) {
     console.error(`Dropdown with ID ${dropdownId} not found.`);
     return;
   }
-
+  // Effacement du du contenu existant
   dropdown.innerHTML = "";
 
   options.forEach((option) => {
     const optionElement = document.createElement("p");
-
+    // Check du type d'option
     if (typeof option === "string") {
       optionElement.textContent = option.toLowerCase();
     } else if (typeof option === "object" && property in option) {
@@ -97,9 +149,11 @@ function updateDropdownOptions(dropdownNumber, options, property) {
       console.error(`Invalid option format: ${option}`);
       return;
     }
+    // Evenement au click sur l'option
     optionElement.onclick = function () {
       selectItem(this);
     };
+    // Ajout de l'élement dans le dropdown
     dropdown.appendChild(optionElement);
   });
 }
---
+++ search/searchByFilter.js
@@ -0,0 +1,193 @@
+/* Fonction de gestion de la sélection ou désélection d'éléments en fonction du filtre */
+function selectItem(selectedElement) {
+  const filterValue = selectedElement.textContent.toLowerCase();
+  // Si le filtre n'est pas présent
+  if (!selectedFilters.some((filter) => filter.toLowerCase() === filterValue)) {
+    // On ajoute le filtre au tableau et une recherche est effectuée
+    selectedFilters.push(filterValue);
+    searchByFilters(selectedFilters);
+  } else {
+    // Récupération de l'élément sélectionné cloné
+    const selectedItemClone = document.querySelector(
+      `.selected-item[data-filter="${filterValue}"]`
+    );
+    if (selectedItemClone) {
+      // Appel de la fonction pour supprimer l'élément sélectionné
+      removeSelectedItem(selectedElement, selectedItemClone);
+      setTimeout(function () {
+        // Timeout pour attendre la mise à jour du DOM avant de déclencher la nouvelle recherche avec les filtres mis à jour
+        searchByFilters(selectedFilters);
+      }, 0);
+    }
+  }
+  // Appel de la fonction pour mettre à jour le visuel des éléments sélectionnés
+  updateSelectedVisuals();
+}
+
+/* Fonction permettant d'ajuster le visuel lors de la sélection des éléments dans les filtres */
+function updateSelectedItemLayout(selectedElement) {
+  const filterValue = selectedElement.textContent.trim().toLowerCase();
+  const svgDropdown = selectedElement.querySelector("svg");
+
+  if (!selectedElement.classList.contains("selected")) {
+    selectedElement.classList.add("selected");
+    selectedElement.style.height = "37px";
+    selectedElement.setAttribute("data-filter", filterValue);
+    const existingClone = document.querySelector(
+      `.selected-item[data-filter="${filterValue}"]`
+    );
+    // S'il n'existe pas de clone avec la même valeur de filtre
+    if (!existingClone) {
+      // Un p est crée
+      selectedItemClone = document.createElement("p");
+      selectedItemClone.textContent = filterValue;
+      // On crée une classe
+      selectedItemClone.classList.add("selected-item");
+      selectedItemClone.setAttribute("data-filter", filterValue);
+      selectedItemClone.onclick = function () {
+        selectItem(this);
+      };
+      // Création d'un conteneur pour accueillir le clone
+      selectedContainer.appendChild(selectedItemClone);
+    }
+  }
+  // S'il n'y a pas d'élément svg
+  if (!svgDropdown) {
+    // On en créé un
+    createDropdownSVG();
+  }
+  // S'il n'y a pas de clone svg
+  if (!selectedItemClone.querySelector("svg")) {
+    // Alors on en crée un
+    createCloneSVG();
+  }
+
+  /* Création de l'UI dans les dropdowns */
+  function createDropdownSVG() {
+    const svgElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+    svgElement.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    svgElement.setAttribute("width", "17");
+    svgElement.setAttribute("height", "17");
+    svgElement.setAttribute("viewBox", "0 0 17 17");
+
+    const circleElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "circle"
+    );
+    circleElement.setAttribute("cx", "8.5");
+    circleElement.setAttribute("cy", "8.5");
+    circleElement.setAttribute("r", "8.5");
+    circleElement.setAttribute("fill", "black");
+
+    const pathElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
+    );
+    pathElement.setAttribute(
+      "d",
+      "M11 11L8.5 8.5M8.5 8.5L6 6M8.5 8.5L11 6M8.5 8.5L6 11"
+    );
+    pathElement.setAttribute("stroke", "#FFD15B");
+    pathElement.setAttribute("stroke-linecap", "round");
+    pathElement.setAttribute("stroke-linejoin", "round");
+    svgElement.appendChild(circleElement);
+    svgElement.appendChild(pathElement);
+    selectedElement.appendChild(svgElement);
+  }
+  // Création des clones svg
+  function createCloneSVG() {
+    if (!selectedItemClone) {
+      console.error("selectedItemClone is undefined");
+    }
+    const svgElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+    svgElement.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    svgElement.setAttribute("width", "14");
+    svgElement.setAttribute("height", "13");
+    svgElement.setAttribute("viewBox", "0 0 14 13");
+    svgElement.setAttribute("fill", "none");
+    const pathElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
+    );
+    pathElement.setAttribute(
+      "d",
+      "M12 11.5L7 6.5M7 6.5L2 1.5M7 6.5L12 1.5M7 6.5L2 11.5"
+    );
+    pathElement.setAttribute("stroke", "#1B1B1B");
+    pathElement.setAttribute("stroke-width", "2.16667");
+    pathElement.setAttribute("stroke-linecap", "round");
+    pathElement.setAttribute("stroke-linejoin", "round");
+    svgElement.appendChild(pathElement);
+    selectedItemClone.appendChild(svgElement);
+  }
+}
+
+/* Fonction de suppression des items sélectionnés */
+function removeSelectedItem(selectedElement, selectedItemClone) {
+  const filterValue = selectedElement.textContent.trim().toLowerCase();
+
+  const index = selectedFilters.indexOf(filterValue);
+  if (index !== -1) {
+    // Utilisation de splice pour enlever l'élément
+    selectedFilters.splice(index, 1);
+  }
+  if (selectedFilters.length === 0) {
+    searchInput.value = "";
+    resetPageState();
+  }
+  selectedElement.classList.remove("selected");
+  selectedElement.style.height = "";
+  selectedElement.querySelector("svg")?.remove();
+
+  if (document.body.contains(selectedItemClone)) {
+    setTimeout(function () {
+      // Timeout pour attendre la mise à jour du DOM
+      selectedItemClone.querySelector("svg")?.remove();
+      selectedItemClone.remove();
+    }, 0);
+  }
+}
+
+/* Fonction de reset lorsque plus aucun élément n'est sélectionné */
+function resetPageState() {
+  updateDropdownOptions(1, allIngredients, "ingredient");
+  updateDropdownOptions(2, allAppliances, "appliance");
+  updateDropdownOptions(3, allUstensils, "ustensil");
+  // Mise à jour cartes et nombre de recettes
+  fillCards(recipes);
+  updateRecipeCount();
+}
+
+/* Fonction de mise à jour des items selectionnés */
+function updateSelectedVisuals() {
+  const containers = [
+    dd1ListContainer,
+    dd2ListContainer,
+    dd3ListContainer,
+    selectedContainer,
+  ];
+
+  // Vérification si les filtres et textes associés sont toujours valides en fonction du filtre sélectionné
+  containers.forEach((container) => {
+    const allSelectedItems = container.querySelectorAll('[class*="selected"]');
+
+    allSelectedItems.forEach((selectedItem) => {
+      const filterValue = selectedItem.getAttribute("data-filter");
+      const selectedItemText = selectedItem.textContent.trim().toLowerCase();
+
+      if (
+        !selectedFilters.includes(filterValue) &&
+        !selectedFilters.includes(selectedItemText)
+      ) {
+        // Suppression des éléments qui ne sont plus valides
+        removeSelectedItem(selectedItem, null, null);
+      }
+    });
+  });
+}
